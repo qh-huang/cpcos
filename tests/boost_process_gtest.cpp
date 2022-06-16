56aa41827353d8e89f2c871ceae87899967d5208
QH Huang <qiao.helloworld@gmail.com>
2022-06-17 00:05:10 +0800

    Add boost::process test

---
+++ tests/boost_process_gtest.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include <boost/asio.hpp>
+#include <boost/process.hpp>
+#include <iostream>
+
+using namespace std;
+
+namespace bp = boost::process;
+
+TEST(BoostTest, SyncProcess)
+{
+  bp::ipstream is;  // reading pipe-stream
+  // error, following gets 'unknown file' failure
+  // bp::system("/sbin/ifconfig", "-a", bp::std_out > is, bp::std_err > is, bp::std_in.close());
+  // TODO: remove 'search_path', which add dependency to boost::filesystem (not header only)
+  bp::child c(bp::search_path("ifconfig"), "-a", bp::std_out > is);
+  std::string line;
+
+  while (c.running() && std::getline(is, line) && !line.empty())
+    cout << line << endl;
+
+  EXPECT_TRUE(true);
+}
